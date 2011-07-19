cc65861b41fcf1bfd80be1d9c6df0963d11b05d8
Nate Yocom <nate@pgina.org>
2011-07-19 16:32:33 -0700

    - move pgina code down a dir - add plugins dir, with bits from pgina prebuilt as 'sdk' - move log4net config into core - add shell for configuration app

--- pGina/src/Core/Logging.cs
+++ pGina/src/Core/Logging.cs
@@ -16,6 +16,13 @@ namespace pGina.Core
         {
             string curPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string l4nConfig = string.Format("{0}\\{1}", curPath, "log4net.config");
+
+            if (!File.Exists(l4nConfig))
+            {
+                string curDir = Directory.GetCurrentDirectory();
+                l4nConfig = string.Format("{0}\\{1}", curDir, "log4net.config");
+            }
+
             XmlConfigurator.ConfigureAndWatch(new FileInfo(l4nConfig));
             LogManager.GetLogger("Startup").InfoFormat("Starting up, log4net configured from: {0}", l4nConfig);
         }
