3bdea487dae35fa14e3f4f6884fb82a654990023
David Wolff <david@pgina.org>
2011-07-22 17:01:22 -0700

    Adding a LdapAuthenticator class (empty for now).

---
+++ Plugins/LdapPlugin/Ldap/LdapAuthenticator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pGina.Plugin.Ldap
+{
+    class LdapAuthenticator
+    {
+
+        public LdapAuthenticator()
+        {
+
+        }
+    }
+}
