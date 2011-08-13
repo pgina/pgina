62ce64a9e84b020722a292d33d456ea5f551ce07
David Wolff <david@pgina.org>
2011-08-12 20:41:51 -0700

    Add drive mapping functions to Abstractions.WindowsApi.
     - Note, these functions have side effects so may not belong in
       SafeNativeMethods.

--- pGina/src/Abstractions/WindowsApi/WindowsApi.cs
+++ pGina/src/Abstractions/WindowsApi/WindowsApi.cs
@@ -65,6 +65,64 @@ namespace Abstractions.WindowsApi
                 /// </summary>
                 CREDUIWIN_PACK_32_WOW = 0x10000000,
             }
+
+            #region mpr.dll enums
+            public enum ResourceScope
+            {
+                RESOURCE_CONNECTED = 1,
+                RESOURCE_GLOBALNET,
+                RESOURCE_REMEMBERED,
+                RESOURCE_RECENT,
+                RESOURCE_CONTEXT
+            }
+
+            public enum ResourceType
+            {
+                RESOURCETYPE_ANY,
+                RESOURCETYPE_DISK,
+                RESOURCETYPE_PRINT,
+                RESOURCETYPE_RESERVED
+            }
+
+            public enum ResourceUsage
+            {
+                RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+                RESOURCEUSAGE_CONTAINER = 0x00000002,
+                RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+                RESOURCEUSAGE_SIBLING = 0x00000008,
+                RESOURCEUSAGE_ATTACHED = 0x00000010,
+                RESOURCEUSAGE_ALL = (RESOURCEUSAGE_CONNECTABLE | RESOURCEUSAGE_CONTAINER | RESOURCEUSAGE_ATTACHED),
+            }
+
+            public enum ResourceDisplayType
+            {
+                RESOURCEDISPLAYTYPE_GENERIC,
+                RESOURCEDISPLAYTYPE_DOMAIN,
+                RESOURCEDISPLAYTYPE_SERVER,
+                RESOURCEDISPLAYTYPE_SHARE,
+                RESOURCEDISPLAYTYPE_FILE,
+                RESOURCEDISPLAYTYPE_GROUP,
+                RESOURCEDISPLAYTYPE_NETWORK,
+                RESOURCEDISPLAYTYPE_ROOT,
+                RESOURCEDISPLAYTYPE_SHAREADMIN,
+                RESOURCEDISPLAYTYPE_DIRECTORY,
+                RESOURCEDISPLAYTYPE_TREE,
+                RESOURCEDISPLAYTYPE_NDSCONTAINER
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            internal class NETRESOURCE
+            {
+                public ResourceScope dwScope = 0; // Ignored by WNetAddConnection2
+                public ResourceType dwType = ResourceType.RESOURCETYPE_DISK;
+                public ResourceDisplayType dwDisplayType = 0; // Ignored by WNetAddConnection2
+                public ResourceUsage dwUsage = 0;  // Ignored by WNetAddConnection2
+                public string lpLocalName = null;
+                public string lpRemoteName = null;
+                public string lpComment = "";  // Ignored by WNetAddConnection2
+                public string lpProvider = null;
+            }
+            #endregion
             #endregion
 
             #region credui.dll
@@ -84,6 +142,16 @@ namespace Abstractions.WindowsApi
             #region ole32.dll
             [DllImport("ole32.dll")]
             public static extern void CoTaskMemFree(IntPtr ptr);
+            #endregion
+
+            #region mpr.dll
+            [DllImport("mpr.dll")]
+            public static extern int WNetAddConnection2(NETRESOURCE netResource,
+                            string password, string username, int flags);
+
+            [DllImport("mpr.dll")]
+            public static extern int WNetCancelConnection2(string name, int flags,
+                            bool force);
             #endregion
         }
 
@@ -125,6 +193,23 @@ namespace Abstractions.WindowsApi
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Map a network drive.
+        /// </summary>
+        /// <param name="unc">The full UNC path.</param>
+        /// <param name="drive">The drive letter (e.g. "Z:", "X:", etc.)</param>
+        /// <param name="user">The username, null if you want the current user.</param>
+        /// <param name="password">The password, null to use the default password.</param>
+        /// <returns>The error code of the WNetAddConnection2 function.</returns>
+        public static int MapNetworkDrive(string unc, string drive, string user, string password)
+        {
+            SafeNativeMethods.NETRESOURCE myNetResource = new SafeNativeMethods.NETRESOURCE();
+            myNetResource.lpLocalName = drive;
+            myNetResource.lpRemoteName = unc;
+            int result = SafeNativeMethods.WNetAddConnection2(myNetResource, password, user, 0);
+            return result;
         }
     }
 }
