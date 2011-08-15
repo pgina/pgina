cb350d59ce25839e03ca0fddaebe566716c479ce
David Wolff <david@pgina.org>
2011-08-14 23:33:45 -0700

    Add script runner plugin initial framework.

---
+++ Plugins/ScriptRunner/ScriptRunner/PluginImpl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using log4net;
+using pGina.Shared.Interfaces;
+
+namespace pGina.Plugin.ScriptRunner
+{
+    public class PluginImpl : IPluginUserSessionHelper, IPluginSystemSessionHelper, IPluginConfiguration
+    {
+        public static readonly Guid ScriptRunnerPluginUuid = new Guid("1632D7AB-858F-4D88-9603-AADD4DEEA847");
+        private ILog m_logger = LogManager.GetLogger("ScriptRunnerPlugin");
+
+        void IPluginSystemSessionHelper.SessionEnding()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPluginSystemSessionHelper.SessionStarted(Shared.Types.UserInformation userInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPluginUserSessionHelper.SessionEnding()
+        {
+            m_logger.DebugFormat("IPluginUserSessionHelper.SessionEnding");
+        }
+
+        void IPluginUserSessionHelper.SessionStarted(Shared.Types.UserInformation userInformation)
+        {
+            m_logger.DebugFormat("IPluginUserSessionHelper.SessionStarted");
+            Script s = new Script();
+            s.File = @"C:\test.bat";
+            s.Run();
+        }
+
+        public string Description
+        {
+            get { return "Plugin for running scripts"; }
+        }
+
+        public string Name
+        {
+            get { return "Script Runner"; }
+        }
+
+        public Guid Uuid
+        {
+            get { return ScriptRunnerPluginUuid; }
+        }
+
+        public string Version
+        {
+            get { return "1.0.0"; }
+        }
+
+        public void Configure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Plugins/ScriptRunner/ScriptRunner/Script.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using log4net;
+
+namespace pGina.Plugin.ScriptRunner
+{
+    class Script
+    {
+        public string File { get; set; }
+        public int Timeout { get; set; }
+        private ILog m_logger = LogManager.GetLogger("Script");
+
+        public Script()
+        {
+            this.Timeout = 300000;
+            this.File = null;
+        }
+
+        public void Run()
+        {
+            if (File == null) return;
+
+            m_logger.InfoFormat("Executing script: {0}", File);
+            System.Diagnostics.ProcessStartInfo startInfo = 
+                new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = File,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo = startInfo;
+            p.EnableRaisingEvents = true;
+            p.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(p_OutputDataReceived);
+            p.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(p_ErrorDataReceived);
+            p.Exited += new EventHandler(p_Exited);
+
+            p.Start();
+            if (p.WaitForExit(Timeout))
+            {
+                m_logger.DebugFormat("Script finished execution successfully.");                
+            }
+            else
+            {
+                m_logger.ErrorFormat("Timeout reached before script finished executing.");
+            }
+            p.Close();
+        }
+
+        void p_Exited(object sender, EventArgs e)
+        {
+            m_logger.DebugFormat("p_Exited");
+        }
+
+        void p_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            m_logger.ErrorFormat(e.Data);
+        }
+
+        void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            m_logger.InfoFormat(e.Data);
+        }
+    }
+}
