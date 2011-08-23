2f295f0dc5aa4306bafed66a469cd43c67bdcf24
David Wolff <david@pgina.org>
2011-08-22 21:45:28 -0700

    Add database logger class

---
+++ Plugins/MySQLLogger/MySQLLogger/DbLogger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace pGina.Plugin.MySqlLogger
+{
+    class DbLogger : IDisposable
+    {
+        private MySqlConnection m_conn;
+        private static string m_ip;
+        private static string m_hostName;
+
+        private static DbLogger()
+        {
+            string m_hostName = Dns.GetHostName();
+            IPHostEntry ipList = Dns.GetHostEntry(m_hostName);
+            m_ip = ipList.AddressList[0].ToString();
+        }
+
+        private DbLogger()
+        {
+            string server = PluginImpl.Settings.Host;
+            int port = PluginImpl.Settings.Port;
+            string userName = PluginImpl.Settings.User;
+            string passwd = PluginImpl.Settings.Password;
+            string database = PluginImpl.Settings.Database;
+
+            string connStr = String.Format("server={0}; port={1};user={2}; password={3}; database={4};",
+                server, port, userName, passwd, database);
+            m_conn = new MySqlConnection(connStr);
+            m_conn.Open();
+        }
+
+        public static DbLogger Connect()
+        {
+            return new DbLogger();
+        }
+
+        public void Log(string message)
+        {
+            string tableName = PluginImpl.Settings.Table;
+            string machine = Environment.MachineName;
+
+            MySqlCommand command = new MySqlCommand(
+                String.Format("INSERT INTO {0}(TimeStamp, Host, Ip, Machine, Message) VALUES (NOW(), {1}, {2}, {3}, {4})", 
+                    tableName, m_hostName, m_ip, machine, message) );
+            int rows = command.ExecuteNonQuery();
+        }
+
+        public void Dispose()
+        {
+            m_conn.Close();
+        }
+    }
+}
--- Plugins/MySQLLogger/MySQLLogger/PluginImpl.cs
+++ Plugins/MySQLLogger/MySQLLogger/PluginImpl.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Text;
 
 using pGina.Shared.Interfaces;
+using pGina.Shared.Settings;
 using log4net;
 
-namespace pGina.Plugin.MySQLLogger
+namespace pGina.Plugin.MySqlLogger
 {
     public class PluginImpl : IPluginSystemSessionHelper, IPluginConfiguration
     {
         public static readonly Guid PluginUuid = new Guid("B68CF064-9299-4765-AC08-ACB49F93F892");
-        private ILog m_logger = LogManager.GetLogger("MySQLLoggerPlugin");
+        private ILog m_logger = LogManager.GetLogger("MySqlLoggerPlugin");
+
+        private static dynamic m_settings = null;
+        internal static dynamic Settings {
+            get { return m_settings; }
+        }
+
+        static PluginImpl()
+        {
+            m_settings = new DynamicSettings(PluginUuid);
+            m_settings.SetDefault("Host", "localhost");
+            m_settings.SetDefault("Port", 3306);
+            m_settings.SetDefault("User", "pGina");
+            m_settings.SetDefault("Password", "secret");
+            m_settings.SetDefault("Database", "pGinaDB");
+        }
 
         public void SessionEnding()
         {
@@ -21,6 +37,18 @@ namespace pGina.Plugin.MySQLLogger
         public void SessionStarted(Shared.Types.UserInformation userInformation)
         {
             m_logger.Debug("SessionStarted()");
+
+            try
+            {
+                using (DbLogger db = DbLogger.Connect())
+                {
+                    db.Log(String.Format("Login: user={0}", userInformation.Username));
+                }
+            }
+            catch (Exception e)
+            {
+                m_logger.ErrorFormat("{0}", e.Message);
+            }
         }
 
         public string Description
