ecb50f1477937f29458c7330004993ef8d939b52
David Wolff <david@pgina.org>
2011-08-28 11:25:59 -0700

    Forgot to include these in previous commit.

---
+++ pGina/src/Core/Messages/DynamicLabelRequestMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace pGina.Core.Messages
+{
+    public class DynamicLabelRequestMessage : MessageBase
+    {
+        public string Name { get; set; }
+
+        public DynamicLabelRequestMessage(dynamic expandoVersion)
+        {
+            FromExpando(expandoVersion);
+        }
+
+        public DynamicLabelRequestMessage()
+        {
+        }
+
+        public override void FromExpando(dynamic expandoVersion)
+        {
+            Name = expandoVersion.Name;
+        }
+
+        public override dynamic ToExpando()
+        {
+            dynamic exp = new ExpandoObject();
+            exp.Name = this.Name;
+            exp.MessageType = (byte) MessageType.DynLabelRequest;
+            return exp;
+        }
+    }
+}
---
+++ pGina/src/Core/Messages/DynamicLabelResponseMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace pGina.Core.Messages
+{
+    public class DynamicLabelResponseMessage : DynamicLabelRequestMessage
+    {
+        public string Text { get; set; }
+
+        public DynamicLabelResponseMessage(dynamic expandoVersion)            
+        {
+            FromExpando(expandoVersion);
+        }
+
+        public DynamicLabelResponseMessage()
+        {
+        }
+
+        public override void FromExpando(dynamic expandoVersion)
+        {
+            base.FromExpando((ExpandoObject) expandoVersion);
+            Text = expandoVersion.Text;
+        }
+
+        public override dynamic ToExpando()
+        {
+            dynamic exp = base.ToExpando();
+            exp.Text = this.Text;
+            exp.MessageType = (byte) MessageType.DynLabelResponse;
+            return exp;
+        }
+    }
+}
