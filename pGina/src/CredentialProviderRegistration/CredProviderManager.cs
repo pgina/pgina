d9e4ae8f9f2ebddb2d451eaa087a6e77f0ba8524
David Wolff <david@pgina.org>
2011-09-05 22:37:07 -0700

    Refactor of CredentialProviderRegistration app.  Not tested yet, may not work properly.

---
+++ pGina/src/CredentialProviderRegistration/CredProviderManager.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Win32;
+
+using log4net;
+
+namespace pGina.CredentialProvider.Registration
+{
+    public abstract class CredProviderManager
+    {
+        public Settings CpInfo { get; set; }
+
+        public CredProviderManager()
+        {
+            CpInfo = new Settings();
+        }
+
+        public static CredProviderManager GetManager()
+        {
+            if (Abstractions.Windows.OsInfo.IsWindows())
+            {
+                if (Abstractions.Windows.OsInfo.IsVistaOrLater())
+                    return new DefaultCredProviderManager();
+                else
+                    return new GinaCredProviderManager();
+            }
+            else
+            {
+                throw new Exception("Must be executed on a Windows OS.");
+            }
+        }
+
+        public void ExecuteDefaultAction()
+        {
+            switch (CpInfo.OpMode)
+            {
+                case OperationMode.INSTALL:
+                    this.Install();
+                    break;
+                case OperationMode.UNINSTALL:
+                    this.Uninstall();
+                    break;
+                case OperationMode.DISABLE:
+                    this.Disable();
+                    break;
+                case OperationMode.ENABLE:
+                    this.Enable();
+                    break;
+            }
+        }
+
+        public abstract void Install();
+        public abstract void Uninstall();
+        public abstract void Disable();
+        public abstract void Enable();
+    }
+
+    public class GinaCredProviderManager : CredProviderManager
+    {
+
+        public override void Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Disable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Enable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DefaultCredProviderManager : CredProviderManager
+    {
+        /*
+         [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{781A7B48-79A7-4fcf-92CC-A6977171F1A8}]
+         @="pGinaCredentialProvider"
+
+         [HKEY_CLASSES_ROOT\CLSID\{781A7B48-79A7-4fcf-92CC-A6977171F1A8}]
+         @="pGinaCredentialProvider"
+
+         [HKEY_CLASSES_ROOT\CLSID\{781A7B48-79A7-4fcf-92CC-A6977171F1A8}\InprocServer32]
+         @="SampleCredUICredentialProvider.dll"
+         "ThreadingModel"="Apartment"
+        */
+
+        private ILog m_logger = LogManager.GetLogger("DefaultCredProviderManager");
+
+        static readonly string PROVIDER_KEY_BASE = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers";
+        static readonly string CLSID_BASE = @"CLSID";
+        static readonly string PROVIDER_KEY_BASE_6432 = @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers";
+
+        // The registry keys
+        string ProviderKey 
+        { 
+            get { return string.Format(@"{0}\{{{1}}}", PROVIDER_KEY_BASE, CpInfo.ProviderGuid.ToString()); }
+        }
+        string ClsidRoot
+        {
+            get { return string.Format(@"{0}\{{{1}}}", CLSID_BASE, CpInfo.ProviderGuid.ToString()); }
+        }
+        string ClsidInProc
+        {
+            get { return string.Format(@"{0}\InprocServer32", this.ClsidRoot); }
+        }
+        string ProviderKey6432
+        {
+            get { return string.Format(@"{0}\{{{1}}}", PROVIDER_KEY_BASE_6432, CpInfo.ProviderGuid.ToString()); }
+        }
+
+        public DefaultCredProviderManager()
+        {
+            // Default Guid for pGina Credential Provider
+            this.CpInfo.ProviderGuid = new Guid("{D0BEFEFB-3D2C-44DA-BBAD-3B2D04557246}");
+        }
+
+        public override void Install()
+        {
+            m_logger.InfoFormat("Installing credential provider {0} {{{1}}}",
+                CpInfo.ShortName,
+                CpInfo.ProviderGuid.ToString());
+
+            // Copy the DLL
+            if (Abstractions.Windows.OsInfo.Is64Bit()) // Are we on a 64 bit OS?
+            {
+                FileInfo x64Dll = DllUtils.Find64BitDll(CpInfo.Path, CpInfo.ShortName);
+                FileInfo x32Dll = DllUtils.Find32BitDll(CpInfo.Path, CpInfo.ShortName);
+                string destination64 = String.Format(@"C:\Windows\System32\{0}.dll", CpInfo.ShortName);
+                string destination32 = String.Format(@"C:\Windows\Syswow64\{0}.dll", CpInfo.ShortName);
+
+                if (x64Dll == null && x32Dll == null)
+                {
+                    throw new Exception("No 64 or 32 bit DLL found in: " + CpInfo.Path);
+                }
+
+                if (x64Dll != null)
+                {
+                    m_logger.DebugFormat("Found 64 bit DLL: {0}", x64Dll.FullName);
+                    m_logger.DebugFormat("   copying to: {0}", destination64);
+                    File.Copy(x64Dll.FullName, destination64, true);
+                }
+                else
+                {
+                    m_logger.Error("WARNING: No 64 bit DLL found.");
+                }
+
+                if (x32Dll != null)
+                {
+                    m_logger.DebugFormat("Found 32 bit DLL: {0}", x32Dll.FullName);
+                    m_logger.DebugFormat("   copying to: {0}", destination32);
+
+                    File.Copy(x32Dll.FullName, destination32, true);
+
+                    // Write registry key for 32 bit DLL
+                    using (RegistryKey key = Registry.LocalMachine.CreateSubKey(this.ProviderKey6432))
+                    {
+                        key.SetValue("", CpInfo.ShortName);
+                    }
+                }
+                else
+                {
+                    m_logger.Error("WARNING: No 32 bit DLL found.");
+                }
+            }
+            else
+            {
+                FileInfo x32Dll = DllUtils.Find32BitDll(CpInfo.Path, CpInfo.ShortName);
+                string destination = String.Format(@"C:\Windows\System32\{0}.dll", CpInfo.ShortName);
+
+                if (x32Dll != null)
+                {
+                    m_logger.DebugFormat("Found 32 bit DLL: {0}", x32Dll.FullName);
+                    m_logger.DebugFormat("   copying to: {0}", destination);
+
+                    File.Copy(x32Dll.FullName, destination, true);
+                }
+                else
+                {
+                    throw new Exception("No 32 bit DLL found in: " + CpInfo.Path);
+                }
+            }
+
+            m_logger.Debug("Writing registry entries...");
+
+            // Write registry values
+            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(this.ProviderKey))
+            {
+                m_logger.DebugFormat("{0} @=> {1}", key.ToString(), CpInfo.ShortName);
+                key.SetValue("", CpInfo.ShortName);
+            }
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(this.ClsidRoot))
+            {
+                m_logger.DebugFormat("{0} @=> {1}", key.ToString(), CpInfo.ShortName);
+                key.SetValue("", CpInfo.ShortName);
+            }
+            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(this.ClsidInProc))
+            {
+                m_logger.DebugFormat("{0} @=> {1}", key.ToString(), CpInfo.ShortName);
+                key.SetValue("", CpInfo.ShortName);
+                m_logger.DebugFormat("{0} {1} => {2}", key.ToString(), "ThreadingModel", "Apartment");
+                key.SetValue("ThreadingModel", "Apartment");
+            }
+        }
+
+        public override void Uninstall()
+        {
+            m_logger.InfoFormat("Uninstalling credential provider {0} {{{1}}}",
+                CpInfo.ShortName,
+                CpInfo.ProviderGuid.ToString());
+
+            string dll = String.Format(@"C:\Windows\System32\{0}.dll", CpInfo.ShortName);
+            string dll6432 = String.Format(@"C:\Windows\Syswow64\{0}.dll", CpInfo.ShortName);
+
+            if (File.Exists(dll))
+            {
+                m_logger.DebugFormat("Deleting: {0}", dll);
+                File.Delete(dll);
+            }
+            if (File.Exists(dll6432))
+            {
+                m_logger.DebugFormat("Deleting: {0}", dll);
+                File.Delete(dll);
+            }
+
+            string guid = "{" + CpInfo.ProviderGuid.ToString() + "}";
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(PROVIDER_KEY_BASE, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Deleting {0}\\{1}", key.ToString(), guid);
+                    key.DeleteSubKey(guid, false);
+                }
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(CLSID_BASE, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Deleting {0}\\{1}\\InprocServer32", key.ToString(), guid);
+                    key.DeleteSubKey(guid + "\\InprocServer32", false);
+                }
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(CLSID_BASE, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Deleting {0}\\{1}", key.ToString(), guid);
+                    key.DeleteSubKey(guid, false);
+                }
+            }
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(PROVIDER_KEY_BASE_6432, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Deleting {0}\\{1}", key.ToString(), guid);
+                    key.DeleteSubKey(guid, false);
+                }
+            }
+        }
+
+        public override void Disable()
+        {
+            m_logger.InfoFormat("Disabling credential provider: {0} {{{1}}}",
+                CpInfo.ShortName,
+                CpInfo.ProviderGuid.ToString());
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.ProviderKey, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Writing {0}: {1} => {2}", key.ToString(), "Disabled", 1);
+                    key.SetValue("Disabled", 1);
+                }
+                else
+                {
+                    m_logger.Error("WARNING: No credential provider registry entry found for that GUID.");
+                }
+            }
+
+            if (Abstractions.Windows.OsInfo.Is64Bit())
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.ProviderKey6432, true))
+                {
+                    if (key != null)
+                    {
+                        m_logger.DebugFormat("Writing {0}: {1} => {2}", key.ToString(), "Disabled", 1);
+                        key.SetValue("Disabled", 1);
+                    }
+                    else
+                    {
+                        m_logger.Error("WARNING: No 32-bit registry entry found with that GUID.");
+                    }
+                }
+            }
+        }
+
+        public override void Enable()
+        {
+            m_logger.InfoFormat("Enabling credential provider: {0} {{{1}}}",
+                CpInfo.ShortName,
+                CpInfo.ProviderGuid.ToString());
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.ProviderKey, true))
+            {
+                if (key != null)
+                {
+                    m_logger.DebugFormat("Deleting {0}: {1}", key.ToString(), "Disabled");
+
+                    if (key.GetValue("Disabled") != null)
+                        key.DeleteValue("Disabled");
+                }
+                else
+                {
+                    m_logger.Error("WARNING: Did not find a registry entry for that GUID.");
+                }
+            }
+
+            if (Abstractions.Windows.OsInfo.Is64Bit())
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.ProviderKey6432, true))
+                {
+                    if (key != null)
+                    {
+                        m_logger.DebugFormat("Deleting {0}: {1}", key.ToString(), "Disabled");
+
+                        if (key.GetValue("Disabled") != null)
+                            key.DeleteValue("Disabled");
+                    }
+                    else
+                    {
+                        m_logger.Error("WARNING: Did not find a (32 bit) registry entry for that GUID.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ pGina/src/CredentialProviderRegistration/DllUtils.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace pGina.CredentialProvider.Registration
+{
+    public class DllUtils
+    {
+        public enum MachineType : ushort
+        {
+            IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
+            IMAGE_FILE_MACHINE_AM33 = 0x1d3,
+            IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+            IMAGE_FILE_MACHINE_ARM = 0x1c0,
+            IMAGE_FILE_MACHINE_EBC = 0xebc,
+            IMAGE_FILE_MACHINE_I386 = 0x14c,
+            IMAGE_FILE_MACHINE_IA64 = 0x200,
+            IMAGE_FILE_MACHINE_M32R = 0x9041,
+            IMAGE_FILE_MACHINE_MIPS16 = 0x266,
+            IMAGE_FILE_MACHINE_MIPSFPU = 0x366,
+            IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,
+            IMAGE_FILE_MACHINE_POWERPC = 0x1f0,
+            IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,
+            IMAGE_FILE_MACHINE_R4000 = 0x166,
+            IMAGE_FILE_MACHINE_SH3 = 0x1a2,
+            IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,
+            IMAGE_FILE_MACHINE_SH4 = 0x1a6,
+            IMAGE_FILE_MACHINE_SH5 = 0x1a8,
+            IMAGE_FILE_MACHINE_THUMB = 0x1c2,
+            IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169,
+        }
+
+        public static bool Is64BitDll(string fullPath)
+        {
+            switch (GetDllMachineType(fullPath))
+            {
+                case MachineType.IMAGE_FILE_MACHINE_AMD64:
+                case MachineType.IMAGE_FILE_MACHINE_IA64:
+                    return true;
+            }
+            return false;
+        }
+
+        private static MachineType GetDllMachineType(string fullPath)
+        {
+            FileStream fs = null;
+            BinaryReader br = null;
+            try
+            {
+                fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                br = new BinaryReader(fs);
+
+                fs.Seek(0x3c, SeekOrigin.Begin);
+                Int32 peOffset = br.ReadInt32();
+                fs.Seek(peOffset, SeekOrigin.Begin);
+                UInt32 peHead = br.ReadUInt32();
+                if (peHead != 0x00004550) // "PE00" little-endian
+                {
+                    throw new Exception("Unable to find PE header in " + fullPath);
+                }
+
+                MachineType type = (MachineType)br.ReadUInt16();
+                return type;
+            }
+            finally
+            {
+                if (br != null) br.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+
+        public static FileInfo Find64BitDll(string path, string baseName)
+        {
+            // Check path directory
+            string fullPath = String.Format(@"{0}\{1}.dll", path, baseName);
+            if (File.Exists(fullPath))
+            {
+                if (DllUtils.Is64BitDll(fullPath))
+                    return new FileInfo(fullPath);
+            }
+
+            // Check x64 subdirectory
+            fullPath = String.Format(@"{0}\x64\{1}.dll", path, baseName);
+            if (File.Exists(fullPath))
+            {
+                if (DllUtils.Is64BitDll(fullPath))
+                    return new FileInfo(fullPath);
+            }
+
+            return null;
+        }
+
+        public static FileInfo Find32BitDll(string path, string baseName)
+        {
+            // Check path directory
+            string fullPath = String.Format(@"{0}\{1}.dll",path, baseName);
+            if (File.Exists(fullPath))
+            {
+                if (!DllUtils.Is64BitDll(fullPath))
+                    return new FileInfo(fullPath);
+            }
+
+            // Check Win32 subdirectory
+            fullPath = String.Format(@"{0}\Win32\{1}.dll", path, baseName);
+            if (File.Exists(fullPath))
+            {
+                if (!DllUtils.Is64BitDll(fullPath))
+                    return new FileInfo(fullPath);
+            }
+
+            return null;
+        }
+    }
+}
