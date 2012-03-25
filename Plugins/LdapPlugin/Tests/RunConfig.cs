f8d74a67f2fc068d6686fd22c80e07d11e03ff8c
David Wolff <david@pgina.org>
2012-03-25 13:53:52 -0700

    Add test app that runs the plugin configuration.  Makes it easier to test the configuration UI, don't need to run the main pGina configuration app.

--- Plugins/LdapPlugin/Tests/RunConfig.cs
+++ Plugins/LdapPlugin/Tests/RunConfig.cs
@@ -30,8 +30,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Reflection;
+using System.Windows.Forms;
 
-using Xunit;
 using pGina.Plugin.Ldap;
 using pGina.Shared.Interfaces;
 using pGina.Shared.Settings;
@@ -39,57 +39,17 @@ using pGina.Shared.Types;
 
 namespace pGina.Plugin.Ldap.Tests
 {
-
-    public class Fixture
+    static class Program
     {
-        public Fixture()
+        /// <summary>
+        /// Runs the LDAP plugin configuration.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            pGina.Shared.Logging.Logging.Init();
-        }
-    }
-
-    public class TestPlugin :IUseFixture<Fixture>
-    {
-        private LdapPlugin plugIn;
-        private Shared.Types.SessionProperties properties = new Shared.Types.SessionProperties(Guid.NewGuid());
-        private Shared.Types.UserInformation userInfo = new Shared.Types.UserInformation();
-
-        public TestPlugin()
-        {
-            plugIn = new LdapPlugin();
-
-            // Add a UserInformation object as pGina would            
-            properties.AddTrackedSingle<Shared.Types.UserInformation>(userInfo);
-
-            dynamic settings = new pGinaDynamicSettings(LdapPlugin.LdapUuid);
-
-            settings.LdapHost = new string[] { "192.168.51.100", "192.168.56.101" };
-            settings.LdapPort = 636;
-            settings.UseSsl = true;
-            settings.RequireCert = true;
-            settings.ServerCertFile = @"c:\slapd.cer";
-            settings.DoSearch = true;
-            settings.SearchContexts = new string[] { "ou=Stuff,dc=example,dc=com", "ou=People,dc=example,dc=com" };
-            settings.SearchFilter = "(&(uid=%u)(objectClass=posixAccount))";
-            settings.DnPattern = "uid=%u,dc=example,dc=com";
-            settings.SearchDN = "cn=search user,dc=example,dc=com";
-            settings.SearchPW = "secret";
-        }
-
-        [Fact]
-        public void TestLogin01()
-        {
-            // Test a basic login.
-            userInfo.Username = "doej";
-            userInfo.Password = "secret";
-            BooleanResult result = plugIn.AuthenticateUser(properties);
-
-            Assert.True( result.Success );
-        }
-
-        public void SetFixture(Fixture data)
-        {
-            // Not used, yet...   
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new pGina.Plugin.Ldap.Configuration());
         }
     }
 }
