7ed8c3b30f90866f4bc819e74e7b7137864cb37c
MutonUfoAI <extr0wurst@gmail.com>
2015-02-04 10:07:03 +0100

    LocalMachine adapt default settings
    - prevent default local group membership mirroring

--- Plugins/LocalMachine/Settings.cs
+++ Plugins/LocalMachine/Settings.cs
@@ -9,8 +9,8 @@
 		* Redistributions in binary form must reproduce the above copyright
 		  notice, this list of conditions and the following disclaimer in the
 		  documentation and/or other materials provided with the distribution.
-		* Neither the name of the pGina Team nor the names of its contributors 
-		  may be used to endorse or promote products derived from this software without 
+		* Neither the name of the pGina Team nor the names of its contributors
+		  may be used to endorse or promote products derived from this software without
 		  specific prior written permission.
 
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -38,19 +38,19 @@ namespace pGina.Plugin.LocalMachine
         private static dynamic m_settings = new pGinaDynamicSettings(PluginImpl.PluginUuid);
 
         static Settings()
-        {            
+        {
             // Authentication step settings:
-            m_settings.SetDefault("AlwaysAuthenticate", true);           // Auth regardless of other plugins auth step
+            m_settings.SetDefault("AlwaysAuthenticate", false);           // Auth regardless of other plugins auth step
 
             // Authorization step settings
             m_settings.SetDefault("AuthzLocalAdminsOnly", false);        // Prevent non-admins
             m_settings.SetDefault("AuthzLocalGroupsOnly", false);        // Require group membership in following list
             m_settings.SetDefault("AuthzLocalGroups", new string[] { }); // Only users in these groups (in their UserInformation, not in SAM!) can be authorized
-            m_settings.SetDefault("AuthzApplyToAllUsers", true);         // Authorize *all* users according to above rules, if false - non-admin/group checks are only done for users *we* authenticated            
-            m_settings.SetDefault("MirrorGroupsForAuthdUsers", true);    // Load users groups from local SAM into userInfo
-            
-            // Gateway step settings                        
-            m_settings.SetDefault("GroupCreateFailIsFail", true);        // Do we fail gateway if group create/add fails?            
+            m_settings.SetDefault("AuthzApplyToAllUsers", true);         // Authorize *all* users according to above rules, if false - non-admin/group checks are only done for users *we* authenticated
+            m_settings.SetDefault("MirrorGroupsForAuthdUsers", false);    // Load users groups from local SAM into userInfo
+
+            // Gateway step settings
+            m_settings.SetDefault("GroupCreateFailIsFail", true);        // Do we fail gateway if group create/add fails?
             m_settings.SetDefault("MandatoryGroups", new string[] { });  // *All* users are added to these groups (by name)
             m_settings.SetDefault("RemoveProfiles", false);              // Do we remove accounts/profiles after logout?
             m_settings.SetDefault("ScramblePasswords", false);           // Do we scramble users passwords after logout?
@@ -64,7 +64,7 @@ namespace pGina.Plugin.LocalMachine
 
         public static dynamic Store
         {
-            get { return m_settings; }            
+            get { return m_settings; }
         }
     }
 }
