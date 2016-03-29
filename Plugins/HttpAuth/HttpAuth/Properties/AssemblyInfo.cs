387e936c51c6c20bc4bd21c396a9d0f54d4c8c28
MutonUfoAI <extr0wurst@gmail.com>
2016-03-29 14:27:04 +0200

    update version and copyright

--- Plugins/HttpAuth/HttpAuth/Properties/AssemblyInfo.cs
+++ Plugins/HttpAuth/HttpAuth/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HttpAuth")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("github vencax")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HttpAuth")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Václav Klecanda 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.9.9.0")]
+[assembly: AssemblyFileVersion("3.9.9.0")]
