b33fbd46f057ee4b1e0e62ca1dfab09ed0cdb76c
MutonUfoAI <extr0wurst@gmail.com>
2016-03-29 14:39:11 +0200

    update version and copyright

--- Plugins/Kerberos/Kerberos/Properties/AssemblyInfo.cs
+++ Plugins/Kerberos/Kerberos/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kerberos")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Seth Walsh")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("University of Utah")]
 [assembly: AssemblyProduct("Kerberos")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © University of Utah 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.5.0")]
-[assembly: AssemblyFileVersion("3.2.5.0")]
+[assembly: AssemblyVersion("3.9.9.0")]
+[assembly: AssemblyFileVersion("3.9.9.0")]
