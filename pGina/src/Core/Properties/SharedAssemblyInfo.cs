be8a74abd17c04e7e144616c0f04c509d6b5a7d1
MutonUfoAI <extr0wurst@gmail.com>
2017-04-04 12:41:29 +0200

    move to 3.9.9.8

--- pGina/src/Core/Properties/SharedAssemblyInfo.cs
+++ pGina/src/Core/Properties/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.9.9.7")]
-[assembly: AssemblyFileVersion("3.9.9.7")]
+[assembly: AssemblyVersion("3.9.9.8")]
+[assembly: AssemblyFileVersion("3.9.9.8")]
